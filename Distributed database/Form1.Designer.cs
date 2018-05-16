ac7618a8c88f09bfd3102d8b0e19252809779997
georgesamuel96 <georges012441@gmail.com>
2018-05-16 13:40:39 +0200

    commit

---
+++ Distributed database/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Distributed_database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.find = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(103, 51);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(75, 23);
+            this.find.TabIndex = 10;
+            this.find.Text = "Find";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 121);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Distributed database/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections;
+using System.IO;
+
+
+namespace Distributed_database
+{
+    public partial class Form1 : Form
+    {
+        string[] files;
+        string query;
+        List<string> doc = new List<string>();
+        Dictionary<string, List<string>> dic = new Dictionary<string, List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+
+                files = Directory.GetFiles(fbd.SelectedPath, "*.txt");
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    List<string> lst = new List<string>();
+                    string temp = File.ReadAllText(files[i]);
+                    splitString(temp, lst);
+                    dic.Add(Path.GetFileName(files[i]), lst);
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            query = textBox1.Text;
+            string str1 = "", str2 = "", op = "", st;
+            int z = 0, x = 0, y = 0, flag = 0, ft = 1;
+
+            for (int j = 0; j < dic.Count; j++)
+            {
+                str1 = ""; str2 = ""; op = "";
+                st = Path.GetFileName(files[j]);
+                ft = 1;
+                z = 0; x = 0; y = 0; flag = 0;
+                for (int i = 0; i < query.Length; i++)
+                {
+                    if (query[i] == ' ')
+                    {
+
+                        if (z == 2)
+                        {
+                            x = 0; y = 0;
+                            for (int k = 0; k < dic[st].Count; k++)
+                            {
+                                if (dic[st][k] == str1)
+                                    x = 1;
+                                if (dic[st][k] == str2)
+                                    y = 1;
+                            }
+                            flag = checkFlag(ref ft, x, y, flag, op);
+                            z = 0;
+                            str2 = op = "";
+
+                        }
+                        z++;
+                    }
+                    else
+                    {
+                        if (z == 0)
+                            str1 += query[i];
+                        else if (z == 1)
+                            op += query[i];
+                        else
+                            str2 += query[i];
+                    }
+                }
+                y = 0;
+                for (int k = 0; k < dic[st].Count; k++)
+                {
+                    if (dic[st][k] == str1)
+                        x = 1;
+                    if (dic[st][k] == str2)
+                        y = 1;
+                }
+                flag = checkFlag(ref ft, x, y, flag, op);
+                z = 0;
+                str2 = op = "";
+                if (flag == 1)
+                {
+                    listBox1.Items.Add(st);
+                }
+            }
+        }
+
+        public void splitString(string str, List<string> doc)
+        {
+            string st = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ' || str[i] == '\r' || str[i] == '\n')
+                {
+                    if (st.Length > 0)
+                        doc.Add(st);
+                    st = "";
+                }
+                else
+                    st = st + str[i];
+            }
+            if (st.Length > 0)
+                doc.Add(st);
+        }
+
+        public bool findWord(string str, List<string> doc)
+        {
+            for (int i = 0; i < doc.Count; i++)
+            {
+                if (doc[i] == str)
+                    return true;
+            }
+            return false;
+        }
+
+        public int checkFlag(ref int ft, int x, int y, int flag, string op)
+        {
+            if (ft == 1)
+            {
+                ft = 0;
+                flag = flag | x;
+                if (op[0] == '&')
+                    flag = flag & y;
+                else if (op[0] == '|')
+                    flag = flag | y;
+            }
+            else
+            {
+                if (op[0] == '&')
+                    flag = flag & y;
+                else if (op[0] == '|')
+                    flag = flag | y;
+            }
+            return flag;
+        }
+    }
+}
